f68e4909169465235f10c93dacd206943c88fb8a
Udi Mazor <udimazor@ip-10-100-56-132.eu-west-1.compute.internal>
2022-11-02 09:58:58 +0200

    First commit, tailwinf install, scss install

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className="p-4 text-lime-400">This is a test</div>
         <a
           className="App-link"
           href="https://reactjs.org"
